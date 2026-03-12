f73f7e14a93a96fdca9d20021bbe4da4b6abb1b7
Daniel Marcos Fuella <daniel.marcos.fuella@hotmail.com>
2026-03-12 13:30:40 +0100

    Actualizar LanguageSelector y desplegable responsive

--- src/components/ContextMovieCard.jsx
+++ src/components/ContextMovieCard.jsx
@@ -1,21 +1,27 @@
-import { useEffect, useState } from "react"
-import { get } from "../data/httpClient"
-import { MovieCard} from "../components/MovieCard"
-import "../components/ContextMovieCard.css"
+    import { useEffect, useState } from "react"
+    import { get } from "../data/httpClient"
+    import { MovieCard} from "../components/MovieCard"
+    import "../components/ContextMovieCard.css"
+    import { LanguageSelector } from "./LanguageSelector"
 
-export function ContextMovieCard(){
-const [movies, SetMovies] = useState([]);
-useEffect(()=>{
-    get("/discover/movie").then((data)=>{
-        SetMovies(data.results)
-        console.log(data)
+    export function ContextMovieCard(){
+    const [movies, SetMovies] = useState([]);
+    const [language, setLanguage] = useState("es-ES");
+    useEffect(()=>{
+        get(`/discover/movie?language=${language}`).then((data)=>{
+            SetMovies(data.results)
+            console.log(data)
 
-    })
-}, [])
-    return(<ul className="container">
-        {movies.map((movie)=>(
-            <MovieCard key={movie.id} movie={movie} />
+        })
+    }, [language])
+        return(
+            <div>
+            <LanguageSelector setLanguage={setLanguage} language={language} />
+        <ul className="container">
+            {movies.map((movie)=>(
+                <MovieCard key={movie.id} movie={movie} />
 
-        ))}
-    </ul>)
-}
+            ))}
+        </ul>
+        </div>)
+    }
---
+++ src/components/LanguageSelector.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useRef } from "react";
+import esFlag from "../img/es.png";
+import enFlag from "../img/en.png";
+import frFlag from "../img/fr.png";
+import deFlag from "../img/de.png";
+import itFlag from "../img/it.png";
+import "./LanguageSelector.css";
+
+
+const languages = [
+  { code: "es-ES", flag: esFlag, name: "Español" },
+  { code: "en-US", flag: enFlag, name: "English" },
+  { code: "fr-FR", flag: frFlag, name: "Français" },
+  { code: "de-DE", flag: deFlag, name: "Deutsch" },
+  { code: "it-IT", flag: itFlag, name: "Italiano" }
+];
+
+export function LanguageSelector({ setLanguage, language }) {
+  const [open, setOpen] = useState(false);
+  const containerRef = useRef(null);
+
+  const currentLanguage = languages.find(lang => lang.code === language);
+
+  // Cerrar menú si se hace click fuera
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (containerRef.current && !containerRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  return (
+    <div ref={containerRef} className="language-selector">
+      <button
+        className="language-selector-button"
+        onClick={() => setOpen(!open)}
+      >
+        <img src={currentLanguage.flag} alt={currentLanguage.name} />
+        {currentLanguage.name} ▼
+      </button>
+
+      <ul className={`language-selector-menu ${open ? "open" : ""}`}>
+        {languages.map(lang => (
+          <li key={lang.code}>
+            <button
+              onClick={() => {
+                setLanguage(lang.code);
+                setOpen(false);
+              }}
+            >
+              <img src={lang.flag} alt={lang.name} />
+              {lang.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
